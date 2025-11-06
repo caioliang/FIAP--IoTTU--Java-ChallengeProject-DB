f6cae4a70c539dfa87af6fcacc4ab822dd25fc87
Caio Liang <caioliang2@gmail.com>
2025-11-06 17:14:00 -0300

    Ajustando scripts de Migration

--- src/main/resources/db/migration/V4__Insert_admin_user.sql
+++ src/main/resources/db/migration/V4__Insert_admin_user.sql
@@ -1,7 +1,7 @@
 -- Insere o primeiro usuário como ADMIN
 -- Email: admin@iottu.com.br
 -- Senha: admin123
-INSERT INTO TB_USUARIO (nome_usuario, email_usuario, senha_usuario, role)
+INSERT INTO TB_USUARIO (nome_usuario, email_usuario, senha_usuario, perfil)
 SELECT 'Administrador', 'admin@iottu.com.br', '$2a$12$RJbKWxHj3.gsFZgvTT8M4OwnHFReJdf7C2sW9M1P6fO9Y486FfjWG', 'ADMIN'
 FROM DUAL
 WHERE NOT EXISTS (
